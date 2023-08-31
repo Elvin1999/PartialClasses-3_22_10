f3b2c7d4c4b63c7e5565b54cd65dc982cd521c4f
camalzadeelvin <camalzadeelvin@STHQ012D-01>
2023-08-31 10:22:20 +0400

    Add project files.

---
+++ ConsoleApp3/BankAccountGen.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public partial class BankAccount
+    {
+        public BankAccount(int id, string? accountName)
+        {
+            Id = id;
+            AccountName = accountName;
+        }
+
+        public BankAccount()
+        {
+            Console.WriteLine($"Id : {Id} - Your account {AccountName} opened");
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Started Program");
+            Console.WriteLine($"Id : {Id} - Your account {AccountName} opened");
+        }
+
+        partial void ShowAccount()
+        {
+            Console.WriteLine(Id);
+            Console.WriteLine(AccountName);
+        }
+
+        public void ShowAccountCall()
+        {
+            ShowAccount();
+        }
+
+        partial void UpdateAccount()
+        {
+            Console.WriteLine("Some update happened");
+        }
+
+        public void UpdateAccountCall()
+        {
+            UpdateAccount();
+        }
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp3
+{
+  
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount(100,"IBAZA1234567854");
+            Console.WriteLine(account.AccountSwift);
+            account.Start();
+
+
+            account.ShowAccountCall();
+            account.UpdateAccountCall();
+
+        }
+    }
+}
